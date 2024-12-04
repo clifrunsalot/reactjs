62b69dedc849967486f055b51b77410a7abbf006
Clifton Hudson <clifrunsalot@gmail.com>
2024-12-04 16:16:06 -0500

    ReactJs Practice

---
+++ grocery-list/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/** Describe the About component */
+function About() {
+  return (
+    <div>
+        <h1 className="title center">About</h1>
+        <p>This is a simple shopping list application.</p>
+    </div>
+  )
+}
+
+export default About
