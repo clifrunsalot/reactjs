0788b377413b720d550d050cc074fa711fb99875
Clifton Hudson <clifrunsalot@gmail.com>
2024-12-05 18:44:58 -0500

    ReactJs Practice

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -13,36 +13,34 @@ function App() {
     e.preventDefault();
 
     // The inputRef.current.value is a string, so we need to convert it to a number.
-    // Must use the 'setResult' function to update the value of 'result' in the state.
-    setResult(Number(inputRef.current.value) + Number(result));
+    // Then, assign the sum of the input value and the current result to a new variable.
+    const newResult = Number(inputRef.current.value) + Number(result);
 
-    // Then assign the value of 'result' to the resultRef.current.value to display it on the screen.
-    resultRef.current.value = result;
+    // Must use the 'setResult' function to update the value of 'result' in the state.
+    setResult(newResult);
   }
 
   function minus(e) {
     e.preventDefault();
-    setResult(Number(result) - Number(inputRef.current.value));
-    resultRef.current.value = result;
+    const newResult = Number(result) - Number(inputRef.current.value);
+    setResult(newResult);
   }
 
   function times(e) {
     e.preventDefault();
-    setResult(Number(inputRef.current.value) * Number(result));
-    resultRef.current.value = result;
+    const newResult = Number(inputRef.current.value) * Number(result);
+    setResult(newResult);
   }
 
   function divide(e) {
     e.preventDefault();
-    setResult(Number(result) / Number(inputRef.current.value));
-    resultRef.current.value = result;
+    const newResult = Number(result) / Number(inputRef.current.value);
+    setResult(newResult);
   }
 
   function resetResult(e) {
-    
     e.preventDefault();
     setResult(0);
-    resultRef.current.value = result;
   }
 
   function resetInput(e) {
@@ -56,9 +54,15 @@ function App() {
         <h1>Simplest Working Calculator</h1>
       </div>
       <form>
-        <p ref={resultRef}>Result: {result}</p>
-        <input pattern="[0-9]" type="text" ref={inputRef} placeholder='Enter a number'/>
+        {/* To display the result, use the state variable 'result' not the ref resultRef.*/}
+        <p>Result: {result}</p>
+        <input pattern="[0-9]" type="text" ref={inputRef} placeholder='Enter a number' />
         <br />
+
+        {/* Add buttons for each operation. 
+          Suggestion: Time permitting, add error handling for cases when there is no
+          value in the input field, perhaps an alert.*/}
+
         <button onClick={plus}>add</button>
         <button onClick={minus}>subtract</button>
         <button onClick={times}>multiply</button>
