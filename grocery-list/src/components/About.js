7976f13caaa026a360e6837242286f743b76e5f1
Clifton Hudson <clifrunsalot@gmail.com>
2024-12-05 13:26:19 -0500

    ReactJs Practice

--- grocery-list/src/components/About.js
+++ grocery-list/src/components/About.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import ReactPlayer from 'react-player'; // react-player needs to be installed locally to use it
+import '../styles/About.css';
 
 /** Describe the About component */
 function About() {
+
+  const vidUrl = "https://www.youtube.com/watch?v=N5SSWumNAp8";
+
   return (
     <div>
-        <h1 className="title center">About</h1>
-        <p>This is a simple shopping list application.</p>
+      <h1 className="title center">About</h1>
+      <p>This is a simple shopping list application.</p>
+      <div className="player-wrapper">
+        <ReactPlayer className="react-player"
+          url={vidUrl}
+          playing={false}
+          volume={0.5}
+          width="100%"
+          height="100%" />
+      </div>
     </div>
   )
 }
